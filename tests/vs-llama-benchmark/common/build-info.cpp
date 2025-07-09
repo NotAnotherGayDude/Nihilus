11ce574c092a719e2942c3641c231e4140ef85c4
NotAnotherGayDude <177492814+NotAnotherGayDude@users.noreply.github.com>
2025-07-08 23:26:51 -0400

    Testing.

--- tests/vs-llama-benchmark/common/build-info.cpp
+++ tests/vs-llama-benchmark/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 46;
-char const *LLAMA_COMMIT = "3d16715";
+int LLAMA_BUILD_NUMBER = 48;
+char const *LLAMA_COMMIT = "146b9e5";
 char const *LLAMA_COMPILER = " ";
 char const *LLAMA_BUILD_TARGET = "Windows AMD64";
